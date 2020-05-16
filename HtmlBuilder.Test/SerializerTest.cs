3813e382672db483368926db13b03c7bfc8a41a6
MELkey <melkey6@gmail.com>
2020-05-16 13:55:54 +0300

    Add save to file test

--- HtmlBuilder.Test/SerializerTest.cs
+++ HtmlBuilder.Test/SerializerTest.cs
@@ -9,6 +9,7 @@ namespace HtmlBuilder.Test
 {
     public class SerializerTest
     {
+        private readonly string file = "Document.html";
         private IHtmlSerializerFactory serializerFactory;
         private IElementFactory elementFactory;
 
@@ -46,6 +47,72 @@ namespace HtmlBuilder.Test
     </div>
 </div>";
             Assert.AreEqual(result, testResult); 
+        }
+
+        [Test]
+        public void SaveToFileTest()
+        {
+            var document = new Document()
+                .HtmlContainer
+                .AddAttribute(a => a.Lang, l => l.SetValue("en"))
+                .AddAttribute(a => a.ItemScope, @is => @is.SetValue(true))
+                .AddAttribute(a => a.ItemType, it => it.SetValue("https://schema.org"))
+                    .Head
+                        .AddChild(e => e.Link)
+                        .AddAttribute(a => a.Rel, r => r.SetValue("main.css"))
+                .Document.HtmlContainer
+                    .Body
+                        .AddChild(e => e.Header)
+                        .AddChild(e => e.Div)
+                        .AddAttribute(d => d.Class, cls => cls.SetValue("main_container"))
+                            .AddCustomChild("custom_tag")
+                                .AddAttribute(ct => ct.AccessKey, ak => ak.SetValue("custom_key"))
+                                .AddCustomAttribute("custom_attribute", "custom_attribute")
+                                .AddContent("Text in custom element")
+                        .Parent
+                            .AddChild(i => i.Img)
+                    .Parent.Parent.Parent
+                        .AddChild(e => e.Footer)
+                .Document
+                ;
+            using (var fileStream = new FileStream(file, FileMode.Create, FileAccess.Write))
+            {
+                using (var streamWriter = new StreamWriter(fileStream))
+                {
+                    var serializer = new HtmlSerializer(document);
+                    serializer.Serialize(streamWriter);
+                    streamWriter.Flush();
+                    streamWriter.Close();
+                }
+            }
+
+            var testHtmlText =
+@"<!DOCTYPE html>
+<html lang=""en"" itemscope itemtype=""https://schema.org"">
+    <head>
+        <link rel=""main.css""/>
+    </head>
+    <body>
+        <header>
+            <div class=""main_container"">
+                <custom_tag accesskey=""custom_key"" custom_attribute=""custom_attribute"">Text in custom element</custom_tag>
+                <img/>
+            </div>
+        </header>
+        <footer/>
+    </body>
+</html>";
+            var result = string.Empty;
+            using (var fileStream = new FileStream(file, FileMode.Open, FileAccess.Read))
+            {
+                using (var streamReader = new StreamReader(fileStream))
+                {
+                    result = streamReader.ReadToEnd();
+                    streamReader.Close();
+                }
+            }
+
+            Assert.AreEqual(result, testHtmlText);
         }
     }
 }
